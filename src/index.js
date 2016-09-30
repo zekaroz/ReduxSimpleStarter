771f8b539d28f1b9805247fe8550b1a48ed8a19c
José Queirós <zekaroz@gmail.com>
2016-09-30 08:43:23 +0100

    Commit para guardar o trabalho dos exercicios.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+// this is how we import our home made components
+import SearchBar from './components/search_bar.js';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+// this is the API key for youtube API
+const API_KEY = 'AIzaSyCKpWl_BfFctVHhyG7TKg9j2cWWhOybNbg';
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// 1. We are creating a new componente and it should produce some HTML
+
+
+// const is ES6 sintax : const is a constant that can never be reassing later
+// this is a class, Class App is also a component that is used as a constructor.
+ const App = () => {
+    // this return is JSX
+    return (
+      <div>
+        <SearchBar/>
+      </div>
+    )
+  }
+
+// 2. then the HTML must be places in the DOM
+ReactDOM.render(<App/>, document.querySelector('.container'));
